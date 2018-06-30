4d255fed78ba7bef92b47f674c8bd551ac8f45b3
RogerV <roger.dewayne@gmail.com>
2018-06-30 12:15:26 -0700

    changed program name to 'genfib' (instead of class name of Main)

--- src/main/java/spartan/react_pipe/Main.java
+++ src/main/java/spartan/react_pipe/Main.java
@@ -12,7 +12,7 @@ import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class Main {
-  private static final String clsName = GeneratorIterator.class.getSimpleName();
+  private static final String progname = "genfib";
   private static final ForkJoinPool executor = new ForkJoinPool();
 
   private static final class ForkJoinPool extends java.util.concurrent.ForkJoinPool {
@@ -23,7 +23,7 @@ public class Main {
       return pool -> {
         final ForkJoinWorkerThread t = defaultForkJoinWorkerThreadFactory.newThread(pool);
         t.setDaemon(true);
-        t.setName(String.format("%s-pool-thread-#%d", clsName, workerThreadNbr.getAndIncrement()));
+        t.setName(String.format("%s-pool-thread-#%d", progname, workerThreadNbr.getAndIncrement()));
         return t;
       };
     };
@@ -33,46 +33,47 @@ public class Main {
   }
 
   public static void main(String[] args) {
-    System.out.printf("%s: Generate Fibonacci Sequence values%n", clsName);
+    System.out.printf("%s: Generate Fibonacci Sequence values%n", progname);
 
     final double maxCeiling = args.length > 0 ? Double.parseDouble(args[0]) : 30 /* default */;
 
     final Subscriber.Iterator<Double> src = new GeneratorIterator<>(
-            () -> System.out.printf("%s: [%s] source data generator done%n", clsName, Thread.currentThread().getName()));
+            () -> System.out.printf("%s: [%s] source data generator done%n", progname, Thread.currentThread().getName()));
 
     final Subscriber.Publisher<Double> publisher = src.getPublisher();
 
     final ForkJoinTask<?> fut = executor.submit(() -> {
       final Stream<?> srcStrm = Subscriber.stream(src);
       final String currThrdName = Thread.currentThread().getName();
-      srcStrm.forEach(item -> System.out.printf("%s: [%s] %s%n", clsName, currThrdName, item));
+      srcStrm.forEach(item -> System.out.printf("%s: [%s] %s%n", progname, currThrdName, item));
     });
 
-    System.out.printf("%s: DEBUG: generator lambda invoked for max ceiling value of: %s%n", clsName, maxCeiling);
+    System.out.printf("%s: DEBUG: generator lambda invoked for max ceiling value of: %s%n", progname, maxCeiling);
 
     final Function<Double, Long> generateFibonacciSequence = ceiling -> {
+      final double max_ceiling = ceiling;
       long publishCallCount = 0;
       try {
         long count = 0;
         double j = 0, i = 1;
         publisher.publish(j);
         count++;
-        if (ceiling <= j) return publishCallCount;
+        if (max_ceiling <= j) return publishCallCount;
         publisher.publish(i);
         count++;
-        if (ceiling == i) return publishCallCount;
+        if (max_ceiling == i) return publishCallCount;
         for (; ; ) {
           double tmp = i;
           i += j;
           j = tmp;
-          if (i > ceiling) break;
+          if (i > max_ceiling) break;
           publisher.publish(i);
           count++;
         }
         publishCallCount = count;
       } catch (InterruptedException e) {
         System.err.printf("%s [%s] Fibonacci Sequence generation interrupted%n",
-                clsName, Thread.currentThread().getName());
+                progname, Thread.currentThread().getName());
       } finally {
         publisher.done();
       }
@@ -82,7 +83,7 @@ public class Main {
     final long countOfGeneratedNumbers = generateFibonacciSequence.apply(maxCeiling);
 
     System.out.printf("%s: [%s] %d Fibonacci Sequence numbers generated%n",
-            clsName, Thread.currentThread().getName(), countOfGeneratedNumbers);
+            progname, Thread.currentThread().getName(), countOfGeneratedNumbers);
 
     fut.join();                              // insure that forkJoinTask is in fully completed state
     final Throwable e = fut.getException();  // get any exception that may have been thrown on forkJoinTask
