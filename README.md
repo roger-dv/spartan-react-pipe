c6a4e4be760ce9494e2265a57d66f284970104c3
roger-dv <roger.dewayne@gmail.com>
2018-06-29 17:19:59 -0700

    initial project checkin

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spartan React-Pipe
+
---
+++ src/main/java/spartan/react_pipe/Main.java
@@ -0,0 +1,7 @@
+package spartan.react_pipe;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hellow world");
+  }
+}
